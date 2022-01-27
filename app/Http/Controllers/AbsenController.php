9eba55e0a91aeb0f3252ba241500eac7c436cffd
Kvinnnnn <Khevila31@gmail.com>
2022-01-27 12:38:15 +0700

    tampilanlogin

---
+++ app/Http/Controllers/AbsenController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AbsenController extends Controller
+{
+    public function tombolabsen()
+    {
+        return view("tombolabsen");
+    }
+}
