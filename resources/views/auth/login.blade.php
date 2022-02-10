9c81c314f964d8f1d81a9b81b322224de761e043
dheaap123 <dheaangraini0812@gmail.com>
2022-02-10 11:15:13 +0700

    tampilan login baru

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,90 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Membuat Desain Form Login HTML Dengan CSS - www.kelasprogrammer.com</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <style>
+        body{
+     font-family: sans-serif;
+     background:  #ccfff2;
+}
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+#logo{
+    width: 100%;
+    margin:auto;
+    padding:auto;
+    padding-bottom: 20px;
+}
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+#container{
+    width: 360px;
+    background: white;
+    margin:180px auto;
+    padding: 30px 20px;
+    border-radius: 6px;
+}
+input[type=text],
+input[type=password]{
+    font-size: 15px;
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    box-sizing: border-box;
+    border: 2px solid #ccc;
+    -webkit-transition: 0.5s;
+    transition: 0.5s;
+    outline: none;
+    background-size: 30px;
+    background-position: 5px 5px;
+    background-repeat: no-repeat;
+    padding-left: 40px;
+    border-radius: 5px;
+}
+input[type=text]{
+    background-image: url('gambar/user.png');
+}
+input[type=password]{
+    background-image: url('gambar/password.png');
+}
+        
+input[type=text]:focus,
+input[type=password]:focus {
+    border: 2px solid #555;
+}
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+input[type=submit],
+input[type=reset]{
+    font-size: 15px;
+    background: #009973;
+    color: white;
+    border: white 3px solid;
+    border-radius: 5px;
+    padding: 12px 20px;
+    cursor:pointer;
+    margin-top: 10px;
+}
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+input[type=submit]:hover,
+input[type=reset]:hover{
+    opacity:0.9;
+}
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+input[type=reset]{
+    background:  #999999;
+}
+     </style>
+</head>
+<body>
+     <div id="container">
+        <form>
+            <img id="logo" src="gambar/logo.png">
+            <label for="fname">Username:</label>
+            <input type="text" name="Username" placeholder="Masukan Username">
+            <label for="lname">Password:</label>
+            <input type="password" name="Password" placeholder="Masukan Password">
+            <input type="submit" value="Login">
+            <input type="reset" value="Reset">
+          </form>
+     </div>
+</body>
+</html>
